6c35bc938007f86fad34283dda61fa0996d96cb2
evie-and-eevees <bwkammerzell@my.waketech.edu>
2021-12-04 22:29:49 -0500

    add board.py

---
+++ board.py
@@ -0,0 +1,12 @@
+line1 = ['-', '-', '-']
+line2 = ['-', '-', '-']
+line3 = ['-', '-', '-']
+lines = [line1, line2, line3]
+
+
+def print_board():
+    for x in lines:
+        print(" ".join(x))
+
+
+print_board()
